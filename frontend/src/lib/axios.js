e719855e4e6c1ec4635f7d85e028a4859b9e6214
Ashutosh Gangwar <ashutoshgangwar246gmail.com@Ashutoshs-MacBook-Air.local>
2026-09-20 20:42:52 +0530

    Configure frontend production API

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
-
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api"
+    : "https://backend-hstd.onrender.com/api";
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
   withCredentials: true, // send cookies with the request
